7c3fb0074377d14a4a4437313d09669b8c690a61
Myer <mcckeyer002@outlook.com>
2021-05-15 22:09:32 -0400

    clean code.

--- clock.cpp
+++ clock.cpp
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
+#define ANGLE_PER_HOUR  360 / 12
+#define ANGLE_PER_MIN   360 / 60
+
 float degree(int hour, int min)
 {
-  float angle_hour = hour * 30;
+  float angle_hour = hour * ANGLE_PER_HOUR;
   angle_hour += min * 0.5;
   printf("angle_hour: %f\n", angle_hour);
-  float angle_min = min * 6;
+  float angle_min = min * ANGLE_PER_MIN;
   float res = fabs(angle_hour - angle_min);
   if (res > 180) {
     res = 360 - res;
