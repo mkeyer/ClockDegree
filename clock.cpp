70391d37c8fb375e13376c4c8d87decbc35c5aaf
Myer <mcckeyer002@outlook.com>
2021-05-15 22:02:47 -0400

    combination of add another line, disable first line, add real line and remove function comment and clean code.

--- clock.cpp
+++ clock.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
+#define ANGLE_PER_HOUR  360 / 12
+#define ANGLE_PER_MIN   360 / 60
+
 float degree(int hour, int min)
 {
-  //angle_hour - angle_min
-  float angle_hour = hour * 30;
+  float angle_hour = hour * ANGLE_PER_HOUR;
   angle_hour += min * 0.5;
   printf("angle_hour: %f\n", angle_hour);
-  float angle_min = min * 6;
+  float angle_min = min * ANGLE_PER_MIN;
   float res = fabs(angle_hour - angle_min);
   if (res > 180) {
     res = 360 - res;
@@ -17,7 +19,9 @@ float degree(int hour, int min)
 
 int main(int argc, char *argv[])
 {
-  printf("%f\n", degree(1, 5));
-  printf("a new line");
-  return 0;
+    printf("%f\n", degree(1, 5));
+    printf("a new line2");
+    printf("the real adding line");
+
+    return 0;
 }
